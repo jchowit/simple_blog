b68d0e25decd64396c65dc840f0e0ece3291af95
John Chow <jchowit@hotmail.com>
2025-11-17 09:43:52 +1100

    Initial commit

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+./bin/rails assets:precompile
+./bin/rails assets:clean
+
+bundle exec rails db:migrate
+bundle exec rails db:seed
